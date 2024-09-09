67dece37602bbe6557b55f1839550fae2ba0eef7
carladanieleferreira <daniele.fersan@gmail.com>
2024-09-09 17:01:22 -0300

    Criação da Escola Senai - Classe Pessoa

---
+++ src/main/java/com/poo/EscolaSenai/Professor.java
@@ -0,0 +1,5 @@
+package com.poo.EscolaSenai;
+
+public class Professor {
+
+}
---
+++ src/test/java/com/poo/EscolaSenai/EscolaSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.EscolaSenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
